211ddb1c2f3be921cc7e4060d9b745dc85e1f7a2
Sevgin Ibryam <skate@LAPTOP-FGP3J5BF>
2021-08-10 21:06:09 +0300

    Added styles for hightlighting current menu

--- src/app/navbar/navbar-side/navbar-side.component.ts
+++ src/app/navbar/navbar-side/navbar-side.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { NavigationEnd, NavigationError, Router } from '@angular/router';
+
+const HIGHTLIGHT_CLASS = 'hightlight';
 
 @Component({
   selector: 'app-navbar-side',
   templateUrl: './navbar-side.component.html',
   styleUrls: ['./navbar-side.component.scss']
 })
-export class NavbarSideComponent implements OnInit {
+export class NavbarSideComponent implements AfterViewInit {
+  @ViewChildren('menuElement')
+  menuElements: QueryList<ElementRef>
 
-  constructor() { }
+  // TODO: Make home route global constant
+  homeRoute: string = '/';
+  searchRoute: string = '/categories';
+  libraryRoute: string = '/library';
+  likedRoute: string = '/liked';
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private renderer2: Renderer2
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.router.events.subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        const currentRoute = this.router.url;
+        this.higthlightCurrentRouteMenu(currentRoute);
+      }
+      else if (e instanceof NavigationError) {
+        // TODO: Throw error
+      }
+    });
   }
 
+  private higthlightCurrentRouteMenu(currentRoute: string): void {
+    this.menuElements.forEach(currentElement => {
+      if (currentElement.nativeElement['id'] === currentRoute) {
+        this.renderer2.addClass(currentElement.nativeElement, HIGHTLIGHT_CLASS);
+      }
+      else {
+        this.renderer2.removeClass(currentElement.nativeElement, HIGHTLIGHT_CLASS);
+      }
+    });
+  }
 }
