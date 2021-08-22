bb65b131d496826265ed1395e8bdb8993b86c806
Sevgin Ibryam <skate@LAPTOP-FGP3J5BF>
2021-08-22 16:23:50 +0300

    Refactored code

--- src/app/playlist-songs/playlist-songs/playlist-songs.component.ts
+++ src/app/playlist-songs/playlist-songs/playlist-songs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Playlist } from 'src/app/models/playlist/playlist';
 
@@ -11,9 +11,13 @@ export class PlaylistSongsComponent implements OnInit {
 
   playlist: Playlist;
 
+  @ViewChild('header')
+  header: ElementRef;
+
   constructor(
-    private route: ActivatedRoute
-    ) { }
+    private route: ActivatedRoute,
+    private renderer2: Renderer2
+  ) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(params => {
